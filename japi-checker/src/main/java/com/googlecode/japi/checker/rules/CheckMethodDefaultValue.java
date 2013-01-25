d58a1d05ba1f7b7a98463d0b071132eac470d70c
Tomas Rohovsky <trohovsk@redhat.com>
2013-01-25 18:48:31 +0100

    Added check of the default value of an annotation type method.

---
+++ japi-checker/src/main/java/com/googlecode/japi/checker/rules/CheckMethodDefaultValue.java
@@ -0,0 +1,28 @@
+package com.googlecode.japi.checker.rules;
+
+import com.googlecode.japi.checker.Reporter;
+import com.googlecode.japi.checker.Reporter.Level;
+import com.googlecode.japi.checker.Reporter.Report;
+import com.googlecode.japi.checker.Rule;
+import com.googlecode.japi.checker.model.JavaItem;
+import com.googlecode.japi.checker.model.MethodData;
+
+/**
+ * 
+ * @author Tomas Rohovsky
+ *
+ */
+public class CheckMethodDefaultValue implements Rule {
+
+    @Override
+    public void checkBackwardCompatibility(Reporter reporter, JavaItem reference, JavaItem newItem) {
+
+    	if (((MethodData) reference).getDefaultValue() != null && ((MethodData) newItem).getDefaultValue() == null) {
+			reporter.report(new Report(Level.ERROR, "The default value of the "
+					+ reference.getItemType() + " "
+					+ reference
+					+ " has been removed.", 
+					reference, newItem));
+        }
+    }
+}
