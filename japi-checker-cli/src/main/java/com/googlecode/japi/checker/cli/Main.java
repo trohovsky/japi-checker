83a8c4aaa1eac08761b32b02313f9b65a1d5804a
Tomas Rohovsky <trohovsk@redhat.com>
2013-06-24 14:31:53 +0200

    CLI is now based on args4j. Added option for visibility limited parsing of bytecode.

--- japi-checker-cli/src/main/java/com/googlecode/japi/checker/cli/Main.java
+++ japi-checker-cli/src/main/java/com/googlecode/japi/checker/cli/Main.java
@@ -4,6 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
 import com.googlecode.japi.checker.BCChecker;
 import com.googlecode.japi.checker.ClassDataLoader;
 import com.googlecode.japi.checker.ClassDataLoaderFactory;
@@ -13,85 +18,112 @@ import com.googlecode.japi.checker.Utils;
 import com.googlecode.japi.checker.model.ClassData;
 import com.googlecode.japi.checker.model.Scope;
 
-
 /**
  * 
  * @author Tomas Rohovsky
- *
+ * 
  */
 public class Main {
 
-	public static void main(String[] args) {
+    @Argument(index = 0, metaVar = "REFERENCE", usage = "reference version of library (JAR archive or directory containing classes)", required = true)
+    private File referenceArtifact;
+    @Argument(index = 1, metaVar = "NEW", usage = "new version of library (JAR archive or directory containing classes)", required = true)
+    private File newArtifact;
+    @Option(name = "-b", aliases = "-bin", usage = "report binary incompatibilities only")
+    private boolean reportBinaryIncmpatibilitiesOnly = false;
 
-		if (args.length < 2) {
-			System.out.println("Usage: japi-checker-cli REFERENCE_LIBRARY NEW_LIBRARY [-bin]");
-			System.out.println("Check API and ABI compatiblity of Java libraries.");
-			System.out.println();
-			System.out.println("  REFERENCE_LIBRARY - reference library, JAR archive or directory containing classes");
-			System.out.println("  NEW_LIBRARY - new library, JAR archive or directory containing classes");
-			System.out.println("  -bin    check only binary compatibility (default - source and binary compatibility)");
-			return;
-		}
-		
-		// proceeding of arguments
-		/*List<File> referenceClasspath = new ArrayList<File>();
-		List<File> newArtifactClasspath = new ArrayList<File>();
-		List<AntPatternMatcher> includes = new ArrayList<AntPatternMatcher>();
-		List<AntPatternMatcher> excludes = new ArrayList<AntPatternMatcher>();*/
-		String referencePath = args[0];
-		String newPath = args[1];		
-		File reference = new File(referencePath);
-		File newArtifact = new File(newPath);
-		if (!reference.isDirectory() && !Utils.isArchive(reference)) {
-			throw new IllegalArgumentException(
-					"reference must be either a directory"
-							+ " or a jar (or a zip kind of archive) file");
-		}
-		if (!newArtifact.isDirectory() && !Utils.isArchive(newArtifact)) {
-			throw new IllegalArgumentException(
-					"new artifact must be either a directory"
-							+ " or a jar (or a zip kind of archive) file");
-		}
-		boolean reportSourceIncompatibilities = true;		
-		if (args.length > 2) {
-			reportSourceIncompatibilities = (args[2] == null ? true : (args[2].equals("-bin") ? false : true));
-		}
+    @Option(name = "-a", aliases = "-api", usage = "check API members only")
+    private void setVisibilityLimit(boolean checkAPIMembers) {
+        visibilityLimit = Scope.PROTECTED;
+    }
 
-		// reading of classes
-		ClassDataLoaderFactory<ClassData> classDataLoaderFactory = new DefaultClassDataLoaderFactory();
-		
-		ClassDataLoader<ClassData> referenceDataLoader = classDataLoaderFactory.createClassDataLoader(Scope.PRIVATE);
-		try {
-			referenceDataLoader.read(reference.toURI());
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		// TODO classpath + includes, excludes
-		/*for (File file : this.referenceClasspath) {
-			referenceDataLoader.read(file.toURI());
-		}*/
-		List<ClassData> referenceClasses = referenceDataLoader.getClasses(reference.toURI());//, includes, excludes);
-		
-		ClassDataLoader<ClassData> newArtifactDataLoader = classDataLoaderFactory.createClassDataLoader(Scope.PRIVATE);
-		try {
-			newArtifactDataLoader.read(newArtifact.toURI());
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		// TODO classpath + includes, excludes
-		/*for (File file : this.newArtifactClasspath) {
-			newArtifactDataLoader.read(file.toURI());
-		}*/
-		List<ClassData> newClasses = newArtifactDataLoader.getClasses(newArtifact.toURI());//, includes, excludes);
+    private Scope visibilityLimit = Scope.PRIVATE;
 
-		// checker initialization
-		BCChecker checker = new BCChecker();
-		CLIReporter reporter = new CLIReporter(reportSourceIncompatibilities);
+    public static void main(String[] args) {
+        Main bean = new Main();
+        CmdLineParser parser = new CmdLineParser(bean);
+        try {
+            parser.parseArgument(args);
+            bean.run();
+        } catch (CmdLineException e) {
+            // handling of wrong arguments
+            System.err.println(e.getMessage() + "\n");
+            // print usage
+            System.err.println("Usage: java -jar japi-checker-cli REFERENCE NEW [options...]");
+            System.err.println("Checks API and ABI compatiblity of Java libraries.\n");
+            parser.printUsage(System.err);
+        }
+    }
 
-		// checking
-		checker.checkBackwardCompatibility(reporter, referenceClasses, newClasses);
-		System.out.println("Error count: " + reporter.getCount(Severity.ERROR));
-		System.out.println("Warning count: " + reporter.getCount(Severity.WARNING));
-	}
+    public void run() {
+
+        /*
+        List<File> referenceClasspath = new ArrayList<File>(); List<File>
+        newArtifactClasspath = new ArrayList<File>(); List<AntPatternMatcher>
+        includes = new ArrayList<AntPatternMatcher>();
+        List<AntPatternMatcher> excludes = new ArrayList<AntPatternMatcher>();
+        */
+
+        if (!referenceArtifact.isDirectory()
+                && !Utils.isArchive(referenceArtifact)) {
+            System.err.println("referenceArtifact library "
+                            + referenceArtifact.getName()
+                            + " is not a directory or a JAR (or a ZIP kind of archive) file.");
+            System.exit(2);
+        }
+        if (!newArtifact.isDirectory() && !Utils.isArchive(newArtifact)) {
+            System.err.println("New library "
+                            + newArtifact.getName()
+                            + " is not a directory or a JAR (or a ZIP kind of archive) file.");
+            System.exit(2);
+        }
+
+        // reading of classes
+        ClassDataLoaderFactory<ClassData> classDataLoaderFactory = new DefaultClassDataLoaderFactory();
+
+        ClassDataLoader<ClassData> referenceDataLoader = classDataLoaderFactory.createClassDataLoader(visibilityLimit);
+        try {
+            referenceDataLoader.read(referenceArtifact.toURI());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        // TODO classpath + includes, excludes
+        /*
+        for (File file : this.referenceClasspath) {
+            referenceDataLoader.read(file.toURI()); 
+        }
+        */
+        List<ClassData> referenceClasses = referenceDataLoader.getClasses(referenceArtifact.toURI());// , includes, excludes);
+
+        ClassDataLoader<ClassData> newArtifactDataLoader = classDataLoaderFactory.createClassDataLoader(visibilityLimit);
+        try {
+            newArtifactDataLoader.read(newArtifact.toURI());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        // TODO classpath + includes, excludes
+        /*
+        for (File file : this.newArtifactClasspath) {
+            newArtifactDataLoader.read(file.toURI());
+        }
+        */
+        List<ClassData> newClasses = newArtifactDataLoader.getClasses(newArtifact.toURI());// , includes, excludes);
+
+        // checker initialization
+        BCChecker checker = new BCChecker();
+        CLIReporter reporter = new CLIReporter(!reportBinaryIncmpatibilitiesOnly);
+
+        // checking
+        checker.checkBackwardCompatibility(reporter, referenceClasses,
+                newClasses);
+        System.out.println("Error count: " + reporter.getCount(Severity.ERROR));
+        System.out.println("Warning count: " + reporter.getCount(Severity.WARNING));
+
+        if (reporter.getCount(Severity.ERROR) > 0) {
+            System.exit(1);
+        } else {
+            System.exit(0);
+        }
+    }
 
 }
